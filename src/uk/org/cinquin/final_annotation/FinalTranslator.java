82b0ed27ac253716273e2c2e540b2ed0d936d9d9
Olivier Cinquin <olivier.cinquin@uci.edu>
2016-12-11 19:26:42 -0800

    Fix problem whereby @Final annotation caused spurious "variable might already have been assigned" errors.

--- src/uk/org/cinquin/final_annotation/FinalTranslator.java
+++ src/uk/org/cinquin/final_annotation/FinalTranslator.java
@@ -1,6 +1,7 @@
 package uk.org.cinquin.final_annotation;
 
 import com.sun.source.tree.Tree;
+import com.sun.tools.javac.code.Flags;
 import com.sun.tools.javac.processing.JavacProcessingEnvironment;
 import com.sun.tools.javac.tree.JCTree;
 import com.sun.tools.javac.tree.TreeMaker;
@@ -8,18 +9,17 @@ import com.sun.tools.javac.tree.TreeTranslator;
 
 public class FinalTranslator extends TreeTranslator {
 
-	private final CreateStatement createStatement;
+	TreeMaker treeMaker;
 
 	public FinalTranslator(JavacProcessingEnvironment javacProcessingEnvironment, TreeMaker treeMaker) {
-		createStatement = new CreateStatement(new GetElement(javacProcessingEnvironment), treeMaker);
+		this.treeMaker = treeMaker;
 	}
 
 	@Override
 	public void visitVarDef(JCTree.JCVariableDecl variableDeclaration) {
 		super.visitVarDef(variableDeclaration);
-		if (isFinalAnnotation(variableDeclaration.getModifiers()) /*&& needsTranslation(variableDeclaration)*/) {
-			// result is placed into the AST, replacing the current variable declaration.
-			result = createStatement.apply(variableDeclaration);
+		if (isFinalAnnotation(variableDeclaration.getModifiers())) {
+			variableDeclaration.mods = treeMaker.Modifiers(variableDeclaration.getModifiers().flags | Flags.FINAL);
 		}
 	}
 
